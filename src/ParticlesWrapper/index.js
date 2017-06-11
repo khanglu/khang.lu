9e29a98c15ab15edbb52f699a64244aac9cdeff6
khanglu <lulephuckhang@gmail.com>
2017-06-12 02:42:49 +1000

    Performance tweaking
    - Add why-did-you-update to check for re-renderings

--- src/ParticlesWrapper/index.js
+++ src/ParticlesWrapper/index.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import Particles from "react-particles-js";
 import { particleWebConfig } from "./particle-config";
 
-const ParticlesWrapper = ({ color }) => (
-  <Particles
-    style={{ position: "absolute" }}
-    // Get config string, replace default color with theme color, then parse to object
-    params={JSON.parse(particleWebConfig.replace(/#ffffff/g, color))}
-  />
-);
+class ParticlesWrapper extends PureComponent {
+  render() {
+    return (
+      <Particles
+        style={{ position: "absolute" }}
+        // Get config string, replace default color with theme color, then parse to object
+        params={JSON.parse(
+          particleWebConfig.replace(/#ffffff/g, this.props.color)
+        )}
+      />
+    );
+  }
+}
 
 export default ParticlesWrapper;
