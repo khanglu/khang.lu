a4396fdee8f50b84d1e9ef63188af3508dd1a438
khanglu <lulephuckhang@gmail.com>
2017-06-17 00:05:07 +1000

    Add server-side rendered CSS from styled-components

--- src/server.jsx
+++ src/server.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOMServer from "react-dom/server";
+import { ServerStyleSheet } from "styled-components";
 import { StaticRouter } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import Template from "./template";
@@ -7,18 +8,23 @@ import App from "./App";
 
 export default function serverRenderer({ clientStats, serverStats }) {
   return (req, res, next) => {
+    const sheet = new ServerStyleSheet();
     const context = {};
     const markup = ReactDOMServer.renderToString(
-      <StaticRouter location={req.url} context={context}>
-        <App />
-      </StaticRouter>
+      sheet.collectStyles(
+        <StaticRouter location={req.url} context={context}>
+          <App />
+        </StaticRouter>
+      )
     );
     const helmet = Helmet.renderStatic();
+    const css = sheet.getStyleTags();
 
     res.status(200).send(
       Template({
         markup: markup,
-        helmet: helmet
+        helmet: helmet,
+        css: css
       })
     );
   };
