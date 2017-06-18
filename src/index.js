ad9286c4d8a12b292ea4343fda238ca55e5513dc
khanglu <lulephuckhang@gmail.com>
2017-06-18 22:23:59 +1000

    Add react-snapshot

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-snapshot";
 import App from "./App";
 
-if (process.env.NODE_ENV !== "production") {
-  // Perf tool to check for unnecessary re-renderings
-  const { whyDidYouUpdate } = require("why-did-you-update");
-  whyDidYouUpdate(React);
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
